b875a46ee843e50171b2031237196771c1838822
Pann-ther <projet.chebbah@gmail.com>
2025-11-10 16:54:20 +0100

    feat: ajout DATABASE

---
+++ database/world_sl_25.sql
@@ -0,0 +1,60 @@
+-- Creation de la database
+CREATE DATABASE wsl25;
+
+-- Creation des tables
+-- Men
+
+-- -66kg
+CREATE TABLE m_66kg(
+    id_comp SERIAL PRIMARY KEY,
+    mu  NUMERIC(5,2) NOT NULL,
+    pu NUMERIC(5,2) NOT NULL,
+    dips NUMERIC(5,2) NOT NULL,
+    squat NUMERIC(5,2) NOT NULL
+);
+
+-- -73kg
+CREATE TABLE m_73kg(
+    id_comp SERIAL PRIMARY KEY,
+    mu  NUMERIC(5,2) NOT NULL,
+    pu NUMERIC(5,2) NOT NULL,
+    dips NUMERIC(5,2) NOT NULL,
+    squat NUMERIC(5,2) NOT NULL
+);
+
+-- -80kg
+CREATE TABLE m_80kg(
+    id_comp SERIAL PRIMARY KEY,
+    mu  NUMERIC(5,2) NOT NULL,
+    pu NUMERIC(5,2) NOT NULL,
+    dips NUMERIC(5,2) NOT NULL,
+    squat NUMERIC(5,2) NOT NULL
+);
+
+-- -87kg
+CREATE TABLE m_87kg(
+    id_comp SERIAL PRIMARY KEY,
+    mu  NUMERIC(5,2) NOT NULL,
+    pu NUMERIC(5,2) NOT NULL,
+    dips NUMERIC(5,2) NOT NULL,
+    squat NUMERIC(5,2) NOT NULL
+);
+
+
+-- -94kg
+CREATE TABLE m_94kg(
+    id_comp SERIAL PRIMARY KEY,
+    mu  NUMERIC(5,2) NOT NULL,
+    pu NUMERIC(5,2) NOT NULL,
+    dips NUMERIC(5,2) NOT NULL,
+    squat NUMERIC(5,2) NOT NULL
+);
+
+-- -101kg
+CREATE TABLE m_101kg(
+    id_comp SERIAL PRIMARY KEY,
+    mu  NUMERIC(5,2) NOT NULL,
+    pu NUMERIC(5,2) NOT NULL,
+    dips NUMERIC(5,2) NOT NULL,
+    squat NUMERIC(5,2) NOT NULL
+);
